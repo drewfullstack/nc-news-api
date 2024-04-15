0a2601310c4636ee9b65e75dcdd6961f90b24c40
Andrew Lowes <drewfullstack@gmail.com>
2024-04-15 13:48:10 +0100

    completes task 3 - GET /api

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
 const app = express();
-
-app.use(express.json());
+const endpoints = require("./endpoints.json");
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", (req, res) => {
+  res.status(200).send(endpoints);
+});
 
 app.all("*", (request, response, next) => {
   response.status(404).send({ message: "endpoint not found" });
