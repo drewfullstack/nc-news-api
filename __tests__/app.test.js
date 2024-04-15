647b34d0dd9acb7e9fb970cd8d74169ad14cbe0e
Andrew Lowes <drewfullstack@gmail.com>
2024-04-15 16:30:32 +0100

    completes task 5 - GET /api/articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -73,3 +73,28 @@ describe("GET /api/articles/:article_id", () => {
       });
   });
 });
+
+describe("GET /api/articles", () => {
+  test("status: 200 - responds with an array of all articles, sorted by date in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles).toHaveLength(13);
+        expect(body.articles).toBeSortedBy("created_at", {
+          descending: true,
+        });
+        body.articles.forEach((article) => {
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+          expect(typeof article.comment_count).toBe("number");
+          expect(article.body).toBeUndefined();
+        });
+      });
+  });
+});
--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,4 +1,4 @@
-const { fetchArticle } = require("../models/articles.models");
+const { fetchArticle, fetchArticles } = require("../models/articles.models");
 
 exports.getArticle = (req, res, next) => {
   const { article_id } = req.params;
@@ -15,4 +15,10 @@ exports.getArticle = (req, res, next) => {
     .catch((err) => {
       next(err);
     });
+};
+
+exports.getArticles = (req, res, next) => {
+  fetchArticles().then((articles) => {
+    res.status(200).send({ articles });
+  });
 };
