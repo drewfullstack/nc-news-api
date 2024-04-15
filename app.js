058135f52d2433008cb7ef9015cea42c515ceaf5
Andrew Lowes <drewfullstack@gmail.com>
2024-04-15 17:23:34 +0100

    completes task 6 - GET /api/articles/:article_id/comments

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { getTopics } = require("./controllers/topics.controllers");
 const {
   getArticle,
   getArticles,
+  getComments,
 } = require("./controllers/articles.controllers");
 const endpoints = require("./endpoints.json");
 const app = express();
@@ -17,15 +18,29 @@ app.get("/api/articles/:article_id", getArticle);
 
 app.get("/api/articles", getArticles);
 
-app.all("*", (request, response, next) => {
-  response.status(404).send({ message: "endpoint not found" });
+app.get("/api/articles/:article_id/comments", getComments);
+
+// PSQL ERROR HANDLERS
+app.use((err, req, res, next) => {
+  if (err.code === "23502") {
+    res.status(400).send({ message: "invalid request" });
+  }
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "invalid request" });
+  }
+  next(err);
 });
 
+// CUSTOM ERROR HANDLERS
 app.use((err, request, response, next) => {
   if (err.status && err.message) {
     response.status(err.status).send({ message: err.message });
   }
   next(err);
+});
+
+app.all("*", (request, response, next) => {
+  response.status(404).send({ message: "endpoint not found" });
 });
 
 module.exports = app;
