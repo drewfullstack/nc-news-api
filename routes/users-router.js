099e55980ab9dd19aa2ce087c858376670a53a48
Andrew Lowes <drewfullstack@gmail.com>
2024-04-17 15:21:22 +0100

    adds GET user by username

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getUsers } = require("../controllers/users.controllers");
+const { getUsers, getUser } = require("../controllers/users.controllers");
 const usersRouter = express.Router();
 
 usersRouter.get("/", getUsers);
+usersRouter.get("/:username", getUser);
 
 module.exports = usersRouter;
