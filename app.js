5163dfd11ceea192c236de2f4b4858e19929ba9a
Andrew Lowes <drewfullstack@gmail.com>
2024-05-03 16:47:59 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
+const cors = require("cors");
 const apiRouter = require("./routes/api-router");
-
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
